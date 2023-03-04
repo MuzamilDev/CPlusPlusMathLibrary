991d24195d8b9235702a0c179a8c3c29c4808014
Muzamil Zaman Khan <95095602+MuzamilDev@users.noreply.github.com>
2023-03-04 10:41:55 +0500

    Add files via upload

---
+++ UpperLimit.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int lowerLimit, upperLimit , i;
+
+    cout<<"Enter Lower Limit "<<endl;
+    cin>>lowerLimit;
+
+    cout<<"Enter Upper Limit "<<endl;
+    cin>>upperLimit;
+
+    for(i=lowerLimit; i<=upperLimit; i++){
+        if(i==0){
+        cout<< i <<endl;
+        }
+
+    else{
+        cout<< i <<endl;
+    }
+    }
+    return 0;
+}
